b8e57e5c4d55a309b0cc22e62331b539fd24d7bf
Sanjay Joshi <joshisa@us.ibm.com>
2019-11-21 23:39:49 -0500

    Updating rawgit urls to githack

--- chrome/src/inject/inject.js
+++ chrome/src/inject/inject.js
@@ -1,9 +1,6 @@
-(function() {
-    if (window.hasRun) return;
-    window.hasRun = true;
-    proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/js/inject.js').onSuccess(function (data) {
-      eval(data);
-    }).onFailure(function (status) {
-      alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
-    });
-})();
+proxyXHR.get('https://raw.githack.com/joshisa/huemix-blopscotch/master/js/inject.js').onSuccess(function (data) {
+    eval(data);
+    console.log("Injection code fetched ...");
+}).onFailure(function (status) {
+  alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
+});
